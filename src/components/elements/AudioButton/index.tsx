ff8ecdacdba320c9d3c44eeebef175eed90fd41e
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-10-25 23:51:00 -0300

    feat: add Agent Audio Button play logic

---
+++ src/components/elements/AudioButton/index.tsx
@@ -0,0 +1,40 @@
+import { SpeakerHigh, SpeakerNone } from 'phosphor-react'
+import { useState } from 'react'
+import styles from './styles.module.scss'
+
+interface AudioButtonProps {
+  audioUrl: string
+  durationTime: number
+}
+
+export const AudioButton = ({ audioUrl, durationTime }: AudioButtonProps) => {
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const handlePlayAudio = () => {
+    setIsPlaying(prevState => !prevState)
+
+    setTimeout(() => setIsPlaying(false), durationTime * 1000)
+
+    const audio = new Audio(audioUrl)
+    !isPlaying && audio.play()
+  }
+
+  return (
+    <button
+      className={styles.audio__button}
+      disabled={isPlaying}
+      onClick={handlePlayAudio}
+    >
+      {isPlaying
+        ? <SpeakerHigh
+          className={styles['audio__button--icon']}
+          size={32}
+        />
+        : <SpeakerNone
+        className={styles['audio__button--icon']}
+        size={32}
+        />
+      }
+    </button>
+  )
+}
--- src/pages/agents/[id].tsx
+++ src/pages/agents/[id].tsx
@@ -1,6 +1,7 @@
 import { GetStaticPaths, GetStaticPropsContext } from 'next'
 import Image from 'next/image'
 import { FormattedMessage } from 'react-intl'
+import { AudioButton } from '@App/components/elements/AudioButton'
 import { Agent } from '@App/core/types/IAgent'
 
 interface PathsProps {
@@ -17,9 +18,36 @@ interface IAgents {
 export default function AgentPage ({ agent }: IAgents) {
   return (
     <>
+      <AudioButton
+        audioUrl={agent.voiceLine.mediaList[0].wave}
+        durationTime={agent.voiceLine.maxDuration}
+      />
+
       <h1>{agent.displayName}</h1>
       <p>{agent.description}</p>
-      <Image width={200} height={300} src={agent.fullPortrait}/>
+
+      <Image width={300} height={200} src={agent.fullPortraitV2} />
+
+      <span>{agent.role.description}</span>
+      <Image src={agent.role.displayIcon} width={100} height={100} />
+      <span>{agent.role.displayName}</span>
+
+      <Image src={agent.background} width={100} height={100} />
+
+      {agent.abilities.map(ability => (
+        <div key={ability.slot}>
+          <p>{ability.displayName}</p>
+          <p>{ability.description}</p>
+          <Image src={ability.displayIcon} width={100} height={100} />
+        </div>
+      ))}
+
+      {agent.characterTags?.map(tag => (
+        <div key={tag}>
+          <p>{tag}</p>
+        </div>
+      ))}
+
       <FormattedMessage id='cta' />
     </>
   )
