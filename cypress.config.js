2cafd60d05a0952dcc4b0c1b4e593584cb0e9ac1
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-10-27 22:12:47 -0300

    chore: update yarn.lock

--- cypress.config.js
+++ cypress.config.js
@@ -4,5 +4,5 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     chromeWebSecurity: false
-  },
+  }
 })
--- src/pages/agents/Agents.spec.tsx
+++
@@ -1,5 +0,0 @@
-import { expect, it } from 'vitest'
-
-it('should render', () => {
-  expect(1 + 1).toBe(2)
-})
